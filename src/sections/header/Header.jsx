a4681214b6b15221f0840385bdac221be16c64bf
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-21 12:43:51 +0300

    :zap:Add header component

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import HeaderImg from '../../assets/Header.jpg'
+import data from './data'
+import './header.css'
 
 function Header() {
   return (
-    <section>Header</section>
+    <header>
+      <div className='container header-cont'>
+        <div className='header-prof'>
+          <img src={HeaderImg} alt='Header Image'/>
+        </div>
+        <h3>Brian Yegon</h3>
+        <p>
+        Passionate Full-Stack Developer dedicated to crafting seamless digital experiences. 
+        Proficient in front-end and back-end technologies, I transform ideas into elegant, functional solutions.
+         Collaborative team player with a keen eye for detail and a drive for continuous learning. Let's build the future together 😄 
+        </p>
+        <div className='header-cte'>
+          <a href='#contact' className='btn primary'>Let's Talk</a>
+          <a href='#project' className='btn light'>My work</a>
+        </div>
+        <div className="header-socials">
+          {
+            data.map(item => <a key={item.id} href={item.link}
+            target='-blank' rel='noopener noreferrer'>{item.icon}</a>)
+          }
+        </div>
+      </div>
+    </header>
   )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,11 @@
+import {AiOutlineGithub} from 'react-icons/ai'
+import {AiOutlineMail} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+
+const data = [
+    {id:1, link: 'https://github.com/yegonkimutai', icon: <AiOutlineGithub/>},
+    {id:2, link: '#about', icon: <AiOutlineMail/>},
+    {id:3, link: 'https://www.linkedin.com/in/brian-yegon-kim05/', icon: <AiOutlineLinkedin/>}
+]
+
+export default data
