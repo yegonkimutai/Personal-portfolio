8d2d327565915446be01294e48c157ae7b972944
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-28 20:33:05 +0300

    :art:Update navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -12,27 +12,27 @@ function Navbar() {
 
   return (
     <nav id="navbar">
-      <Link to='home' spy={true} smooth={true} offset={-100} duration={300}
+      <Link to='home' spy={true} smooth={true} offset={-50} duration={500}
       className={activeNav === '#' ? 'active' : ''}>
         <AiOutlineHome />
       </Link>
 
-      <Link to='about' spy={true} smooth={true} offset={-100} duration={500}
+      <Link to='about' spy={true} smooth={true} offset={-50} duration={500}
       className={activeNav === '#about' ? 'active' : ''}>
         <AiOutlineUser />
       </Link>
 
-      <Link to='projects' spy={true} smooth={true} offset={-100} duration={500}
+      <Link to='projects' spy={true} smooth={true} offset={-50} duration={500}
        className={activeNav === '#projects' ? 'active' : ''}>
         <BiBookBookmark />
       </Link>
 
-      <Link to='skills' spy={true} smooth={true} offset={-100} duration={500}
+      <Link to='skills' spy={true} smooth={true} offset={-50} duration={500}
       className={activeNav === '#skills' ? 'active' : ''}>
          <MdOutlineDesignServices />
       </Link>
 
-      <Link to='contact' spy={true} smooth={true} offset={-100} duration={500}
+      <Link to='contact' spy={true} smooth={true} offset={-50} duration={500}
        className={activeNav === '#contact' ? 'active' : ''}>
         <MdConnectWithoutContact />
       </Link>
